487302df307fb812eb8afac37088f7a4d724098b
Taku Ogawa <ogawtaku@gmail.com>
2023-07-04 11:12:00 +0900

    UIを変更

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_app/provider.dart';
@@ -48,13 +49,35 @@ class _MyHomePageState extends ConsumerState<MyHomePage> {
               ref.watch(countProvider).toString(),
               style: Theme.of(context).textTheme.headline4,
             ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                FloatingActionButton(
+                  onPressed: () {},
+                  tooltip: 'Increment',
+                  child: const Icon(CupertinoIcons.plus),
+                ),
+                FloatingActionButton(
+                  onPressed: () {},
+                  tooltip: 'Decrement',
+                  child: const Icon(CupertinoIcons.minus),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: const [
+                Text('1'),
+                Text('2'),
+              ],
+            ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () => ref.read(countProvider.notifier).state++,
         tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        child: const Icon(Icons.refresh),
       ),
     );
   }
