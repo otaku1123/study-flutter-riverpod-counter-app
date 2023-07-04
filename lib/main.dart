e9c1ba991243bd46f99493472d38b11c6cb187eb
Taku Ogawa <ogawtaku@gmail.com>
2023-07-04 10:56:30 +0900

    ConsumerWidgetからConsumerに更新

--- lib/main.dart
+++ lib/main.dart
@@ -21,35 +21,49 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends ConsumerWidget {
+class MyHomePage extends StatelessWidget {
   MyHomePage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          ref.watch(titleProvider),
+        title: Consumer(
+          builder: (BuildContext context, WidgetRef ref, Widget? child) {
+            return Text(
+              ref.watch(titleProvider),
+            );
+          },
         ),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              ref.watch(descriptionProvider),
-            ),
-            Text(
-              ref.watch(countProvider).toString(),
-              style: Theme.of(context).textTheme.headline4,
+            Consumer(builder: (context, ref, child) {
+              return Text(
+                ref.watch(descriptionProvider),
+              );
+            }),
+            Consumer(
+              builder: (context, ref, child) {
+                return Text(
+                  ref.watch(countProvider).toString(),
+                  style: Theme.of(context).textTheme.headline4,
+                );
+              },
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => ref.watch(countProvider.notifier).state++,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      floatingActionButton: Consumer(
+        builder: (context, ref, child) {
+          return FloatingActionButton(
+            onPressed: () => ref.watch(countProvider.notifier).state++,
+            tooltip: 'Increment',
+            child: const Icon(Icons.add),
+          );
+        },
       ),
     );
   }
